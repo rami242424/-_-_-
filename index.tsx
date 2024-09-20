b2a93b2cf48be5fa17d4f075fd545c02d01d4aea
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-20 21:08:12 +0900

    3.0 Call Signatures

--- index.tsx
+++ index.tsx
@@ -1,35 +1,14 @@
-unknown : 어떤 타입인지 모르는 변수는? 
-let a: unknown;
-let b = a + 1 // 'a' is of type 'unknown'
+// function add(a: number, b: number): number
+// function add (a:number, b:number){
+//     return a + b;
+// }
 
-해결
-let a : unknown;
-if(typeof a === "number"){
-    let b = a + 1; // let a: number
-}
-if(typeof a === "string"){
-    let b = a.();
-}
+// const add: (a: number, b: number) => number
+// const add = (a:number, b:number) => a + b
 
-void : 아무것도 return 하지 않는 함수를 대상으로 사용
-function hello(){ // function hello(): void
-    console.log("x");
-}
-const a = hello();
-a.toUpperCase(); // Property 'toUpperCase' does not exist on type 'void'
+// call signature : 함수 위에 마우스를 올렸을 때 보게 되는 것
+type Add = (a:number, b:number) => number;
+const add: Add = (a, b) => a + b; // => number
+const addd: Add = (c, d) => c + d; // => number
+const lala: Add = (x, y) => x + y; // => number
 
-// never : 함수가 절대 return 하지 않을 때 
-function hello() : never {
-    // return "x" // Type 'string' is not assignable to type 'never'
-    throw new Error("xxx"); // 제대로 동작 O > return 하지 않고 오류를 발생시키는 함수
-}
-
-function hi(name: string|number){
-    if(typeof name === "string"){
-        name; // name: string
-    } else if (typeof name === "number"){
-        name; // name: number
-    } else {
-        name; // name: never > 절대 return 되지 않음
-    }
-}
