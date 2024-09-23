081298d6da5d4f39fbc448e2d2b4029aef35d22a
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-24 00:07:16 +0900

    오버로딩 > 어렵

--- index.tsx
+++ index.tsx
@@ -1,14 +1,55 @@
-// function add(a: number, b: number): number
-// function add (a:number, b:number){
-//     return a + b;
-// }
+// call signature shortcut
+type Add = (a: number, b: number) => number
 
-// const add: (a: number, b: number) => number
-// const add = (a:number, b:number) => a + b
+call signature longcut
+type Add = {
+    (a: number, b: number) : number
+}
 
-// call signature : 함수 위에 마우스를 올렸을 때 보게 되는 것
-type Add = (a:number, b:number) => number;
-const add: Add = (a, b) => a + b; // => number
-const addd: Add = (c, d) => c + d; // => number
-const lala: Add = (x, y) => x + y; // => number
+// 오버로딩?
+// 함수가 서로 다른 여러개의 call signature을 가지고 있을 때 발생.
+// 예시1
+type Add = {
+    (a: number, b: number) : number
+    (a: number, b: string) : number
 
+}
+const add : Add = (a, b) => {
+    if(typeof b === "string") return a
+    return a + b
+}
+
+// 예시2
+type Config = {
+    path: string,
+    state: object
+}
+
+type Push = {
+    (path: string): void
+    (config: Config): void
+}
+
+const push: Push = (config) => {
+    if (typeof config === "string") {
+        console.log(config); // string인 경우 처리
+    } else {
+        console.log(config.path); // Config 타입인 경우 처리
+    }
+};
+
+
+// 예시3
+// call 파마리터의 개수도 다른 경우
+type Add = {
+    (a:number, b:number): number
+    (a:number, b:number, c:number): number
+}
+
+const add:Add = (a, b, c?:number) => {
+    if(c) return a + b + c
+    return a + b
+}
+
+add(1,2);
+add(1,2,3);
