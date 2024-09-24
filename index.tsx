9947ab2acb2603f687d750aa64dab9a75c21212a
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-24 18:44:14 +0900

    4.0 classes : constructor

--- index.tsx
+++ index.tsx
@@ -1,10 +1,11 @@
+class Player {
+    constructor(
+        private firstName: string,
+        private lastName: string,
+        public nickname: string
+    ) {}
+}
 
-// type SuperPrint = <T>(arr: T[]) => T
-// const superPrint: SuperPrint = (a) => a[0];
-// 아래 한줄 코드로 변경 가능
-function superPrint<T>(a: T[]){return a[0]}
-
-superPrint([1, 2, 3, 4]);
-superPrint([true, false, true, true]);
-superPrint(["a", "b", "c"]);
-superPrint([1, 5, false, "lala"]);
+const nico = new Player("nico", "last", "니꼬");
+nico.firstName // Property 'firstName' is private and only accessible within class 'Player'.
+nico.nickname
