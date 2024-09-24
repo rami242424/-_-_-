677c0d8780bfa79dab26f888da37f585cf387197
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-24 19:05:58 +0900

    4.0 classes : abstract & protected

--- index.tsx
+++ index.tsx
@@ -1,19 +1,22 @@
-// abstract 추상클래스 : 다른 클래스가 상속받을 수 있는 클래스 (but 직접 새로운 인스턴스를 만들 수는 없다.
-// => const nico = new User("nico", "last", "니꼬"); // Cannot create an instance of an abstract class.)
 abstract class User {
     constructor(
-        private firstName: string,
-        private lastName: string,
-        public nickname: string
+        protected firstName: string,
+        protected lastName: string,
+        protected nickname: string
     ) {}
-    // private getFullName(){
-        getFullName(){
+    abstract getNickName(): void
+
+    getFullName(){
         return `${this.firstName} ${this.lastName}`
     }
 }
 
 class Player extends User {
+    getNickName(){
+        console.log(this.nickname)
+    }
 }
 
 const nico = new Player("nico", "last", "니꼬");
-nico.getFullName // private getFullName()이 되면 Property 'getFullName' is private and only accessible within class 'User' 접근 불가.
+nico.getFullName()
+nico.firstName // Property 'firstName' is protected and only accessible within class 'User' and its subclasses.
