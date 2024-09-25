bcac4cf8e6cd39ca1e16330dd1512c36c05408ab
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-25 22:30:13 +0900

    #4.5 Polymorphism

--- index.tsx
+++ index.tsx
@@ -1,31 +1,29 @@
-// 아래 interface 코드는 `자바스크립트`에서 보여지지 않는다.
-interface User {
-    firstName : string,
-    lastName : string,
-    sayHi(name:string):string   
-    fullName():string
+interface SStorage<T> {
+    [key:string]: T // key가 제한되지 않은 오브젝트를 정의하게 해줌
 }
 
-// implements : extends를 쓸때보다 코드가 더 가벼워진다.
-// User interface 추적불가 : 인터페이스는 타입스크립트에서만 존재하고 자바스크립트에는 존재하지 않는다.
-// interface를 상속할 때에는 프로퍼티를 `private | protected`으로 만들지 못한다. > public 으로만 가능
-
-// interface 추가해보기
-interface Human {
-    health: number
-}
-
-class Player implements User, Human { 
-    constructor(
-        public firstName : string,
-        public lastName  : string,
-        public health : number
-    ){}
-    fullName(){
-        return `${this.firstName} ${this.lastName}`
+class LocalStorage<T> {
+    private storage : SStorage<T> = {}
+    set(key:string, value:T){
+        this.storage[key] = value;
     }
-    sayHi(name: string){
-        return `Hello ${name}. My name is ${this.fullName()}`
+    remove(key:string){
+        delete this.storage[key]
     }
-
+    get(key:string):T {
+        return this.storage[key]
+    }
+    clear(){
+        this.storage = {}
+    }
 }
+
+const stringsStorage = new LocalStorage<string>();
+
+stringsStorage.get("key");
+stringsStorage.set("hello", "how are you")
+
+const booleansStorage = new LocalStorage<boolean>();
+
+booleansStorage.get("key");
+booleansStorage.set("key", true);
