2320a07496f68a6145746a0370e475adf7671050
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-25 17:57:59 +0900

    4.2 Interface : 다양한 type만들기

--- index.tsx
+++ index.tsx
@@ -1,38 +1,72 @@
-type Words = {
-    [key:string]: string // property의 이름은 모르지만 type을 알때 사용하는 방법
+// type 사용방법1 : object식
+// type Player = {
+//     nickname : string,
+//     healthBar: number
+// }
+
+// const nico: Player = {
+//     nickname:"nico",
+//     healthBar: 10
+// }
+
+// type 사용방법2
+// type Food = string;
+// const kimchi:Food = "delecious";
+
+// type 사용방법3
+// type Nickname = string;
+// type Health = number;
+// type Friends = Array<string>
+
+// type Player = {
+//     nickname : Nickname,
+//     healthBar: Health
+// }
+
+// const nico: Player = {
+//     nickname:"nico",
+//     healthBar: 10
+// }
+
+// type 사용방법4
+type Team = "red" | "blue" | "yellow";
+type Health = 1 | 5 | 10;
+
+type Player = {
+    nickname: string,
+    team: Team,
+    health: Health
+}
+
+const nico : Player = {
+    nickname: "nico",
+    // team: "pink", // Type '"pink"' is not assignable to type 'Team'.
+    team : "blue",
+    health: 10
 }
 
 
-class Dict {
-    private words: Words
-    constructor(){
-        this.words = {}
-    }
-    add(word:Word){
-        if(this.words[word.term] === undefined){
-            this.words[word.term] = word.def;
-        }
-    }
-    def(term:string){
-        return this.words[term]
-    }
-    static hello(){
-        return "hello";
-    }
-}
-
-class Word {
-    constructor (
-        public readonly term : string, // readonly를 추가 해줌으로써 값은 보여주나 수정은 불가능하게 만들 수 있다.
-        public readonly def : string
-    ) {}
-}
 
 
-const kimchi = new Word("kimchi", "한국의 음식");
-// kimchi.def = "D"; //Cannot assign to 'def' because it is a read-only property 
-const dict = new Dict()
 
-dict.add(kimchi);
-dict.def("kimchi");
-Dict.hello() // static 메소드
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
