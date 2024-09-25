af734f6d27840e9ac6d39dbd3c0037419a388ed0
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-25 18:07:41 +0900

    4.2Interface: type _interface 비교

--- index.tsx
+++ index.tsx
@@ -1,38 +1,17 @@
-// type 사용방법1 : object식
-// type Player = {
-//     nickname : string,
-//     healthBar: number
-// }
-
-// const nico: Player = {
-//     nickname:"nico",
-//     healthBar: 10
-// }
-
-// type 사용방법2
-// type Food = string;
-// const kimchi:Food = "delecious";
-
-// type 사용방법3
-// type Nickname = string;
-// type Health = number;
-// type Friends = Array<string>
-
-// type Player = {
-//     nickname : Nickname,
-//     healthBar: Health
-// }
-
-// const nico: Player = {
-//     nickname:"nico",
-//     healthBar: 10
-// }
-
-// type 사용방법4
 type Team = "red" | "blue" | "yellow";
 type Health = 1 | 5 | 10;
 
-type Player = {
+// type과 interface
+
+// type
+// type Player = {
+//     nickname: string,
+//     team: Team,
+//     health: Health
+// }
+
+// interface
+interface Player {
     nickname: string,
     team: Team,
     health: Health
@@ -40,7 +19,6 @@ type Player = {
 
 const nico : Player = {
     nickname: "nico",
-    // team: "pink", // Type '"pink"' is not assignable to type 'Team'.
     team : "blue",
     health: 10
 }
