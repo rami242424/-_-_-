daf4ad72ec61e0b192ecba5f60cb8009df9904cf
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-25 23:51:21 +0900

    #5.4 JSDoc

--- TypeChain/src/myPackage.js
+++ TypeChain/src/myPackage.js
@@ -1,7 +1,22 @@
+// @ts-check // TS파일에게 JVS파일을 확인하라고 알리는 것
+
+/**
+ * Initializes the project // 어떤함수인지 설명
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+
 export function init(config){
     return true;
 }
 
+/**
+ * Exits the program
+ * @param {number} code 
+ * @returns {number}
+ */
 export function exit(code){
     return code + 1;
 }
