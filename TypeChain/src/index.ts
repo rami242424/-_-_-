604b7044ecbf3c20d4cbdbb304e64da85d57a0d5
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-26 21:35:59 +0900

    #5.6 DefinitelyTyped

--- TypeChain/src/index.ts
+++ TypeChain/src/index.ts
@@ -1,4 +1,4 @@
-import crypto from "crypto";
+import * as crypto from "crypto";
 
 interface BlockShape {
     hash: string;
@@ -18,5 +18,6 @@ class Block implements BlockShape{
     }
     static calculateHash(prevHash:string, height: number, data: string){
         const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
