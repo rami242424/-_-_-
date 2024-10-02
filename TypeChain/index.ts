d053ef190497ebbe7e5cc10512615367c351d157
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-02 18:54:57 +0900

    #4.0 Classes

--- TypeChain/index.ts
+++ TypeChain/index.ts
@@ -1,11 +1,17 @@
-// what is 객체지향 프로그래밍?
-class  Player {
+// 추상클래스 : 다른 클래스가 상속받을 수 있는 클래스 but 직접 새로운 인스턴스를 만들 수는 없다.
+abstract class User{
     constructor(
         private firstName: string,
         private lastName: string,
         public nickName: string
     ) {}
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+class  Player extends User{
 }
 
 const nico = new Player("nico" , "las", "니꼬");
-// nico.firstName // Property 'firstName' is private and only accessible within class 'Player'
+nico.getFullName() 
