890a82f0f764f0cb47d2b8f0428a4cd1f73d8fa0
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-02 18:21:45 +0900

    #3.4 Conclusions - 1

--- TypeChain/index.ts
+++ TypeChain/index.ts
@@ -1,10 +1,14 @@
-type SuperPrint = {
-    <T>(arr: T[]): T
+// 아래 두 함수 합쳐놓은 것
+function superPrint<T>(a: T[]){
+    return a[0]
 }
 
-const superPrint: SuperPrint = (arr) => arr[0]
+// type SuperPrint = {
+//     <T>(arr: T[]): T
+// }
+// const superPrint: SuperPrint = (arr) => arr[0]
 
-const a = superPrint([1,2,3,4])
+const a = superPrint<number>([1,2,3,4])
 const b = superPrint([true, false, false])
 const c = superPrint(["a", "b"])
 const d = superPrint([1, 2, "a", "b", true])
