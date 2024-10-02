24b6fba2eeb8bbcfbdc8736e22fae649ab7ba479
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-02 21:02:32 +0900

    4.2 interfaces - type사용방법들

--- TypeChain/index.ts
+++ TypeChain/index.ts
@@ -1,17 +1,29 @@
-// 추상클래스 : 다른 클래스가 상속받을 수 있는 클래스 but 직접 새로운 인스턴스를 만들 수는 없다.
-abstract class User{
-    constructor(
-        private firstName: string,
-        private lastName: string,
-        public nickName: string
-    ) {}
-    getFullName(){
-        return `${this.firstName} ${this.lastName}`
-    }
+// 타입 사용방법1
+type Player1 = {
+    nickname: string,
+    healthBar: number
+}
+const nico1 : Player1 = {
+    nickname: "nico1",
+    healthBar: 10
 }
 
-class  Player extends User{
-}
 
-const nico = new Player("nico" , "las", "니꼬");
-nico.getFullName() 
+// 타입 사용방법2
+type Food = string;
+const kimchi : Food = "delicious"
+
+
+// 타입 사용방법3
+type Nickname = string
+type Health = number
+type Friends = Array<string> // type Friends = string[]
+
+type Player2 = {
+    nickname: Nickname,
+    healthBar: Health
+}
+const nico2 : Player2 = {
+    nickname: "nico2",
+    healthBar: 10
+}
