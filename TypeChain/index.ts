b22eebb5548dd4ed0c2121def43b77b86c178b93
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-04 17:09:48 +0900

    4.4 Recap : type vs interface

--- TypeChain/index.ts
+++ TypeChain/index.ts
@@ -1,13 +1,24 @@
-type Team = "red" | "blue" | "black"
-type Health = 1 | 5 | 4
-type Player = {
-    nickname : string,
-    team: Team,
-    health: Health
+// type과 interface 
+// 공통점 : 타입스크립트에게 오브젝트의 모양과 타입을 알려주는 게 목표
+// 차이점 : 이용해서 할 수 있는 것이 다름
+// 타입
+// 인터페이스
+
+
+// type
+type PlayerA = {
+    name: string
+}
+const playerA: PlayerA = {
+    name: "nico"
 }
 
-const nico : Player = {
-    nickname: "nico",
-    team: "blue",
-    health: 4
-}    
+
+// interface
+interface PlayerB {
+    name: string
+}
+
+const playerB: PlayerB = {
+    name: "nico"
+}
