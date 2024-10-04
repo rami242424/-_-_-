1c07fd93bb472e16cecd5a7ed181fb644d5dd5bf
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-04 23:39:02 +0900

    #5.5 Blocks

--- TypeChain/src/index.ts
+++ TypeChain/src/index.ts
@@ -1,1 +1,26 @@
-console.log("ok bye");
+// import * as crypto from "crypto"; // or  tsconfig에서 ` "esModuleInterop": true,` 로 설정하기
+import crypto from "crypto";
+
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+} 
+
+
+// static 함수 : 클래스 안에서 사용하는 함수, 클래스 인스턴스가 없어도 부를 수 있는 함수 (객체지향 프로그래밍)
+
+class Block implements BlockShape{
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash: string, height: number, data: string){
+        const toHash = `${prevHash}${height}${data}`;
+    }
+}
