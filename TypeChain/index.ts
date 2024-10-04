953c5ace06d6f6235dc1896a7f016c7a62a662f8
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-04 17:52:28 +0900

    추상클래스 대체 하는 방법

--- TypeChain/index.ts
+++ TypeChain/index.ts
@@ -1,43 +1,22 @@
-// type과 interface 
-// 공통점 : 타입스크립트에게 오브젝트의 모양과 타입을 알려주는 게 목표
-// 차이점 : 이용해서 할 수 있는 것이 다름
-// 타입 : 타입을 상속하려면 또 다른 타입 하나를 만들어서
-// 인터페이스
+// 추상클래스 대체하는 방법
 
-//?? property에 타입을 추가하고 싶으면?
-
-
-// type
 type PlayerA = {
-    name: string
+    firstName: string
+}
+class User1 implements PlayerA {
+    constructor(
+        public firstName: string
+    ){}
 }
 
-type PlayerAA = PlayerA & { // 상속방법
-    lastName: string
-}
-
-type PlayerAA = { // 타입추가 ❌ Error : Duplicate identifier 'PlayerAA'
-    health: number
-}
-const playerA: PlayerAA = {
-    name: "nico",
-    lastName: "oh"
-}
-
-
-// interface
 interface PlayerB {
-    name: string
+    firstName: string
+}
+class User2 implements PlayerB {
+    constructor(
+        public firstName: string
+    ){}
 }
 
-interface PlayerBB extends PlayerB { // 상속방법
-    lastName: string
-}
-
-interface PlayerBB { // 타입추가 ⭕
-    health: number
-}
-
-const playerB: PlayerB = {
-    name: "nico"
-}
+// 클래스나 오브젝트의 모양을 정의하고 싶으면 > type
+// 그외의 경우 > interface 
