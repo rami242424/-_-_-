2d0c3a01bbe877d6c022404821eb634cf53b7ea9
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-04 23:06:42 +0900

    #5.4 JSDoc

--- TypeChain/src/index.ts
+++ TypeChain/src/index.ts
@@ -1,7 +1,2 @@
-import { init, exit } from 'myPackage';
-
-init({
-    url: "true"
-});
-
-exit(1)
+// "./~" 이 타입스크립트 파일에 "./~" 파일을 불러온다는 뜻
+import {  init, exit } from './myPackage';
--- TypeChain/src/myPackage.js
+++ TypeChain/src/myPackage.js
@@ -1,7 +1,26 @@
+// @ ts-check ->TS 파일에게 JVS파일을 확인하라고 알림
+// JSDoc : 코멘트로 이루어진 문법
+
+
+
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
+
 export function init(config) {
     return true;
 }
 
+/**
+ * Exit the program
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
     return code + 1;
 }
